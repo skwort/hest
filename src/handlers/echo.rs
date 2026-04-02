725a11f376d57db40f9867d1688fd894b8c630bf
Sam Kwort <skwort@skwort.dev>
2026-04-02 22:15:10 +1000

    Integrate xmpp-rs into main
    Allow hest to receive and reply to messages via xmpp.

--- src/handlers/echo.rs
+++ src/handlers/echo.rs
@@ -1,5 +1,7 @@
 use crate::handler::{Action, Handler, Message};
 
+const ECHO_HANDLER_PREFIX: &str = "@echo";
+
 pub struct EchoHandler;
 
 impl Handler for EchoHandler {
@@ -7,11 +9,14 @@ impl Handler for EchoHandler {
         "EchoHandler"
     }
 
-    fn wants(&self, _msg: &Message) -> bool {
-        true
+    fn wants(&self, msg: &Message) -> bool {
+        msg.body.starts_with(ECHO_HANDLER_PREFIX)
     }
 
     fn process(&self, msg: &Message) -> Vec<Action> {
-        vec![Action::Reply(format!("echo: {}", msg.body))]
+        vec![Action::Reply(format!(
+            "echo: {}",
+            msg.body.strip_prefix(ECHO_HANDLER_PREFIX).unwrap().trim()
+        ))]
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,24 @@ mod handlers;
 use handler::{Action, Message, Router};
 use handlers::EchoHandler;
 
-use std::io::{self, BufRead};
+use xmpp::jid::BareJid;
+use xmpp::message::send::MessageSettings;
+use xmpp::muc::room::JoinRoomSettings;
+use xmpp::{Agent, ClientBuilder, ClientType, Event, RoomNick};
 
-fn main() {
+use tokio::signal::ctrl_c;
+
+use std::str::FromStr;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    // XMPP uses TLS; here we setup the crypto provider to enable TLS.
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("failed to install rustls crypto provider");
+
     // Create the router
     let mut router = Router {
         handlers: Vec::new(),
@@ -19,24 +34,91 @@ fn main() {
     // This takes ownership of eh.
     router.handlers.push(Box::new(eh));
 
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let line = match line {
-            Ok(text) => text,
-            Err(e) => {
-                println!("error reading: {}", e);
-                continue;
-            }
-        };
+    // Create the XMPP client
+    let jid = BareJid::from_str("hest@xmpp.skwort.dev").unwrap();
+    let password = "default";
+    let nick = RoomNick::from_str("hest").unwrap();
+    let mut client = ClientBuilder::new(jid, password)
+        .set_client(ClientType::Bot, "hest")
+        .set_default_nick(nick)
+        .build();
 
-        let msg = Message {
-            body: line,
-            from: String::from("stdin"),
-        };
-        for actions in router.dispatch(&msg) {
-            match actions {
-                Action::Reply(text) => println!("reply: {}", text),
-            }
+    log::info!("Connecting...");
+
+    loop {
+        tokio::select! {
+            events = client.wait_for_events() => {
+                for event in events {
+                    handle_events(&router, &mut client, event).await
+                }
+            },
+            _ = ctrl_c() => {
+                log::info!("Disconnecting...");
+                client.disconnect().await.unwrap();
+                break;
+            },
         }
+    }
+}
+
+async fn handle_events(router: &Router, client: &mut Agent, event: Event) {
+    match event {
+        Event::Online => {
+            log::info!("Online.");
+            log::info!("Joining Home");
+            let room = BareJid::from_str("home@conference.xmpp.skwort.dev").unwrap();
+            client
+                .join_room(JoinRoomSettings {
+                    status: Some(("en", "At your behest.")),
+                    ..JoinRoomSettings::new(room.clone())
+                })
+                .await;
+        }
+        Event::Disconnected(e) => {
+            log::info!("Disconnected: {}.", e);
+        }
+        Event::ChatMessage(_id, jid, body, time_info) => {
+            log::info!(
+                "{} {}: {}",
+                time_info.received.time().format("%H:%M"),
+                jid,
+                body
+            );
+
+            // We can skip replayed messages using:
+            // if !time_info.delays.is_empty() {
+            //    return;
+            // }
+            handle_message(router, client, body, jid).await;
+        }
+        Event::RoomMessage(_id, jid, nick, body, time_info) => {
+            // Skip any replayed messages
+            if !time_info.delays.is_empty() {
+                return;
+            }
+            println!(
+                "Message in room {} from {} at {}: {}",
+                jid, nick, time_info.received, body
+            );
+        }
+        _ => {
+            log::debug!("Unimplemented event:\n{:#?}", event);
+        }
+    }
+}
+
+async fn handle_message(router: &Router, client: &mut Agent, body: String, from: BareJid) {
+    let msg = Message {
+        body,
+        from: from.to_string(),
+    };
+    for actions in router.dispatch(&msg) {
+        match actions {
+            Action::Reply(text) => {
+                client
+                    .send_message(MessageSettings::new(from.clone(), &text))
+                    .await
+            }
+        };
     }
 }
